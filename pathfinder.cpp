10b3df5c159ce9fca1a77e05d443e6dee4b0c665
Arun S Ramakrishnan <asramakr@ieng6-201.ucsd.edu>
2016-11-15 15:00:51 -0800

    Partial Pathfinder

---
+++ pathfinder.cpp
@@ -0,0 +1,57 @@
+/**
+ * File: pathfinder.cpp
+ * Author: Arun Ramakrishnan, Alexis Atianzar
+ * Date: 11/18/16
+ * 
+ */
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "ActorGraph.h"
+
+using namespace std;
+
+int main(int argc, char ** argv){
+
+  ActorGraph currentGraph;
+  bool use_weighted_edges = false;
+
+  if(argc != 4){
+    return 1;
+  }
+  //std::filebuf fb1;
+  //fb1.open(argv[1], std::ios::binary);
+  //std::istream castfile(&fb1);
+
+  char flag = *(argv[2]);
+
+  // if 'u' inputed, graph with unweighted edges
+  if (flag == 'u') {
+    use_weighted_edges = false;
+  }
+
+  // if 'w' inputed, graph with weighted edges
+  if (flag == 'w') {
+    use_weighted_edges = true;
+  }
+
+  currentGraph.loadFromFile(argv[1], use_weighted_edges);
+
+
+/*
+  std::filebuf fb2;
+  fb2.open(argv[3], std::ios::binary);
+  std::istream actorfile(&fb2);
+
+  //fb2.close();
+
+  std::filebuf fb3;
+  fb3.open(argv[4], std::ios::binary);
+
+  fb3.close();
+
+  */
+}
